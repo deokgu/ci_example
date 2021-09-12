a14407f95a86bf3d19496dda70a1baa8766fe0e8
deokgu <onefence1994@gmail.com>
2021-09-12 09:17:05 +0900

    fix _test

--- main_test.py
+++ main_test.py
@@ -6,7 +6,7 @@ import main
 class MainTest(unittest.TestCase):
     def test_hellword(self):
         ret = main.helloword("Tets")
-        self.assertEqual(ret, "ASD")
+        self.assertEqual(ret, "ASDASD")
 
 
 if __name__ == "__main__":
